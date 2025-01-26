6983b21c59efa4217ff7bce4bd32beb565b2c4aa
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2025-01-26 02:10:42 -0300

    fix: tests

--- src/utils/get-dates-of-week.spec.ts
+++ src/utils/get-dates-of-week.spec.ts
@@ -1,11 +1,11 @@
 import { getDatesOfWeek } from './get-dates-of-week'
 
 test('get first day of week', () => {
-  const date = '2025-01-19'
+  const date = '2025-01-19T03:00:00'
 
   const datesOfWeek = getDatesOfWeek(date)
 
   expect(datesOfWeek).toHaveLength(7)
-  expect(datesOfWeek[0].getDate()).toEqual(12)
-  expect(datesOfWeek[6].getDate()).toEqual(18)
+  expect(datesOfWeek[0].getDate()).toEqual(19)
+  expect(datesOfWeek[6].getDate()).toEqual(25)
 })
